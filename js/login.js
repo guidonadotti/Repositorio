fdda49a909bae216e7424140d1a8158005a8b01f
GuidoNadotti <gpmnadotti@gmail.com>
2022-08-16 21:42:00 -0300

    Googlen't

--- js/login.js
+++ js/login.js
@@ -1,11 +1,3 @@
-function onSignIn(googleUser){
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
-
 function mailVacío(){
     let mail=document.getElementById("mail")
     mail.style.border= "1px solid rgb(220, 53, 69)"
